6a3475b37149018cbb0cf00920b56b43140a46c3
geithamum <n.g.a.tgaming@gmail.com>
2025-04-26 15:06:28 -0600

    Updated data cleaning algorithm to include filtering for products, and number to day/month/hour conversion

--- DatathonProject/script.py
+++ DatathonProject/script.py
@@ -5,6 +5,23 @@ import re
 # Load your dataset
 df = pd.read_csv("retail_cleaned_data.csv", dtype={"InvoiceNo": "string", "StockCode": "string","Description": "string", "Quantity": int, "UnitPrice": float, "Country": "string", "Revenue": float, "Year": "string", "Month": "string", "DayOfWeek": "string", "Hour": "string", "Year": "string", "Weekday": "string"})
 
+df['Hour'] = df['Hour'].astype(int).apply(
+    lambda hour: (
+        "12 AM" if hour == 0 else
+        f"{hour} AM" if hour < 12 else
+        "12 PM" if hour == 12 else
+        f"{hour - 12} PM"
+    )
+)
+
+df['DayOfWeek'] = df['DayOfWeek'].astype(int).apply(
+    lambda day: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'][day]
+)
+
+df.to_csv('CleanedData.csv', index=False)
+
+cdf = pd.read_csv("CleanedData.csv", dtype={"InvoiceNo": "string", "StockCode": "string","Description": "string", "Quantity": int, "UnitPrice": float, "Country": "string", "Revenue": float, "Year": "string", "Month": "string", "DayOfWeek": "string", "Hour": "string", "Year": "string", "Weekday": "string"})
+
 # Create folders if they don't exist
 os.makedirs('products', exist_ok=True)
 os.makedirs('countries', exist_ok=True)
@@ -18,7 +35,7 @@ selected_columns = [
 
 # Split by description
 
-for description, product_df in df.groupby('Description'):
+for description, product_df in cdf.groupby('Description'):
     safe_description = re.sub(r'[\\/*?:"<>|]', "_", description)
     filename = f'products/{safe_description}.csv'
     product_df_selected = product_df[selected_columns]
@@ -26,7 +43,7 @@ for description, product_df in df.groupby('Description'):
 
     
 
-for description, country_df in df.groupby('Country'):
+for description, country_df in cdf.groupby('Country'):
     safe_description = re.sub(r'[\\/*?:"<>|]', "_", description)
     filename = f'countries/{safe_description}.csv'
     country_df_selected = country_df[selected_columns]
